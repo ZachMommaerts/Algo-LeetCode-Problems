8e1d0749fbcabc75bf2607c114d2ed26e0f40ed6
Zach <zmommaerts@gmail.com>
2022-03-30 21:59:42 -0500

    added valid parenthesis problem

--- easy/14-Longest-Common-Prefix/longest-common-prefix.js
+++ easy/14-Longest-Common-Prefix/longest-common-prefix.js
@@ -1,5 +1,13 @@
 const longestCommonPrefix = (strs) => {
+    let prefix = '';
 
+    for(let i = 0; i < strs.length; i++){
+        const wordArray = strs[i].split('')
+        prefix = `${prefix}${wordArray[i]}`
+        console.log(prefix)
+    }
+
+    return wordArray;
 }
 
 console.log(longestCommonPrefix(["flower","flow","flight"]))
