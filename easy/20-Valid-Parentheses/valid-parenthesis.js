7b6aa213cca6896436b19ddbdff2c5aaaa3354f1
Zach <zmommaerts@gmail.com>
2022-03-31 23:11:37 -0500

    first paranthesis solution

--- easy/20-Valid-Parentheses/valid-parenthesis.js
+++ easy/20-Valid-Parentheses/valid-parenthesis.js
@@ -1,3 +1,20 @@
 const isValid = (s) => {
-    
+    const parenthesisArray = s.split('')
+    let reversePointer;
+
+    for(let i = 0; i < parenthesisArray.length; i++){
+        reversePointer = parenthesisArray.length - 1;
+
+        if(parenthesisArray[i] === '(' && parenthesisArray[reversePointer] !== ')') {
+            return false;
+        } else if (parenthesisArray[i] === '[' && parenthesisArray[reversePointer] !== ']') {
+            return false;
+        } else if (parenthesisArray[i] === '{' && parenthesisArray[reversePointer] !== '}') {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+console.log(isValid('()[]{}'))
