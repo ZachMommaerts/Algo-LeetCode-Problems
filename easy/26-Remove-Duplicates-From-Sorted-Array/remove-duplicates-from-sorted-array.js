c4331d0a1d0bac3e41264031b4ebf0119860126e
Zach <zmommaerts@gmail.com>
2022-04-04 22:45:51 -0500

    added remove element problem

--- easy/26-Remove-Duplicates-From-Sorted-Array/remove-duplicates-from-sorted-array.js
+++ easy/26-Remove-Duplicates-From-Sorted-Array/remove-duplicates-from-sorted-array.js
@@ -5,11 +5,9 @@ const removeDuplicates = (nums) => {
     for (let i = 0; i < nums.length; i++) {
         current = nums[i];
 
-        if (nums[i] !== nums.length -1) {
-            for (let j = 1; j < nums.length; j++) {
-                if(current === nums[j]) {
-                    nums.splice(nums[j], 1)
-                }
+        for (let j = 1; j < nums.length; j++) {
+            if(nums[j] && current === nums[j]) {
+                nums.splice(nums[j], 1)
             }
         }
 
---
+++ easy/27-Remove-Element/remove-element.js
@@ -0,0 +1,5 @@
+const removeElement = (nums, val) => {
+
+}
+
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
