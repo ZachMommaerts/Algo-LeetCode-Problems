19419b4dddfc6c908547613c6dab3bae1e362d78
Zach <zmommaerts@gmail.com>
2022-04-05 21:40:22 -0500

    nearly completed remove element, not working on leetcode

--- easy/27-Remove-Element/remove-element.js
+++ easy/27-Remove-Element/remove-element.js
@@ -1,5 +1,12 @@
 const removeElement = (nums, val) => {
+    for(let i = 0; i < nums.length; i++){
+        if (nums[i] === val) {
+            nums[i] = '_';
+        }
+    }
 
+    const filtered = nums.filter(element => element !== '_')
+    return filtered
 }
 
-console.log(removeElement([0,1,2,2,3,0,4,2], 2))
+console.log(removeElement([3,2,2,3], 3))
